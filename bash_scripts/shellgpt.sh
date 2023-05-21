587a300f516a84f9a0c10ad48f7f378491ceb237
Adrian <adriangryga@gmail.com>
2023-05-21 17:46:19 +0200

    Updated shellgpt script

--- bash_scripts/shellgpt.sh
+++ bash_scripts/shellgpt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-
+echo "Ten skrypt instaluje i synchronizuje CHAT_GPT z terminalem"
+sleep 2
 sudo apt-get update && sudo apt upgrade -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 echo "Zaktualizowano system. Instalacja python3 i python3-pip zakończona powodzeniem"
+sleep 2
 echo "Rozpoczynam instalację SHELL GPT"
-cp .bashrc ~/bashrc_old
+sleep 2
+cp /home/$USER/.bashrc /home/$USER/bashrc_old
+echo "Skopiowano plik .bashrc do lokalizacji /home/$USER/bashrc_old"
+sleep 2
 pip install shell-gpt --user
 echo "Przygotuj klucz API od OpenAI"
-echo "export PATH=$PATH:~/.local/bin:/usr/bin/python3" >> .bashrc
-source .bashrc
+sleep 2
 cd ~/.local/bin && python3 sgpt
+cd
+echo "export PATH=$"PATH":~/.local/bin:/usr/bin/python3" >> .bashrc
+source .bashrc
+echo "Instalacja CHATU GPT zakończona. Możesz zadać mu pytanie komendą sgpt "
+
